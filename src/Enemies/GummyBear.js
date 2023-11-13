2ef0dc8253397cbb820798a9c93d9c2acc05e5c5
Timpan42 <tim@fagerdal.se>
2023-11-13 09:05:35 +0100

    🔥

--- src/Enemies/GummyBear.js
+++ src/Enemies/GummyBear.js
@@ -9,7 +9,6 @@ export default class GummyBear extends Enemy {
         this.lives = Math.floor(Math.random() * (6 - 3 + 1) + 3) + Math.floor(1 * scaling)
         this.damage = 3 + Math.floor(1 * scaling)
         this.points = 20
-        this.color = '#b50d0f'
         this.type = 'gummyBear'
         this.coinMax = 5
         this.coinMin = 2
